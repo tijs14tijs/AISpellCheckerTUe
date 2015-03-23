802615567176ec8e2beec6cd532db3b63f2a697f
Tijs Maas <johanmaas@mbpvanjohanmaas.lan>
2015-03-23 20:23:59 +0100

    Initial commit

---
+++ AISpellCheckerTUe/src/SpellChecker.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class SpellChecker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        boolean inPeach = false; // set this to true if you submit to peach!!!
+        
+        try {
+            CorpusReader cr = new CorpusReader();
+            ConfusionMatrixReader cmr = new ConfusionMatrixReader();
+            SpellCorrector sc = new SpellCorrector(cr, cmr);
+            if (inPeach) {
+                peachTest(sc);
+            } else {
+                nonPeachTest(sc);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
+    }
+    
+    static void nonPeachTest(SpellCorrector sc) throws IOException { 
+            String[] sentences = {
+                "at the hme locations there were traces of water"
+            };
+            
+            for(String s0: sentences) {
+                System.out.println("Input : " + s0);
+                String result=sc.correctPhrase(s0);
+                System.out.println("Answer: " +result);
+                System.out.println();
+            }
+    }
+    
+    static void peachTest(SpellCorrector sc) throws IOException {
+            Scanner input = new Scanner(System.in);
+            
+            String sentence = input.nextLine();
+            System.out.println("Answer: " + sc.correctPhrase(sentence));  
+    } 
+}
---
+++ AISpellCheckerTUe/src/SpellCorrector.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+
+public class SpellCorrector {
+    final private CorpusReader cr;
+    final private ConfusionMatrixReader cmr;
+    
+    final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz'".toCharArray();
+    
+    
+    public SpellCorrector(CorpusReader cr, ConfusionMatrixReader cmr) 
+    {
+        this.cr = cr;
+        this.cmr = cmr;
+    }
+    
+    public String correctPhrase(String phrase)
+    {
+        if(phrase == null || phrase.length() == 0)
+        {
+            throw new IllegalArgumentException("phrase must be non-empty.");
+        }
+            
+        String[] words = phrase.split(" ");
+        String finalSuggestion = "";
+        
+        /** CODE TO BE ADDED **/
+        
+        return finalSuggestion.trim();
+    }
+    
+    public double calculateChannelModelProbability(String suggested, String incorrect) 
+    {
+         /** CODE TO BE ADDED **/
+        
+        return 0.0;
+    }
+         
+      
+    public HashSet<String> getCandidateWords(String word)
+    {
+        HashSet<String> ListOfWords = new HashSet<String>();
+        
+        /** CODE TO BE ADDED **/
+        
+        return cr.inVocabulary(ListOfWords);
+    }          
+}
